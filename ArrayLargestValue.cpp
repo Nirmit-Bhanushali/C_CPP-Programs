10d9333e176f317d7b262f0360ac249a6e5b47fc
Nirmit <nirmit.bhanushali@gmail.com>
2026-03-04 11:57:05 +0530

    add ArrayLargestValue.cpp to find the largest value from user input

---
+++ ArrayLargestValue.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5];
+    cout<<"Enter 5 numbers"<<endl;
+    for(int i=0;i<5;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"The largest value you entered is:"<<endl;
+    int max=arr[0];
+    for(int i=1;i<5;i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    cout<<max<<endl;
+    return 0;
+}
