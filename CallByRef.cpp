faec73331337400fb09689187ee1b5a3a23b5a4a
Nirmit <nirmit.bhanushali@gmail.com>
2026-03-04 11:56:10 +0530

    add CallByRef.cpp to demonstrate swapping of two integers using call by reference

---
+++ CallByRef.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void swap(int &a, int &b)
+{
+    cout << "Before swapping a=" << a << " b=" << b;
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "\nAfter swapping a=" << a << " b=" << b;
+}
+int main()
+{
+    int x = 10, y = 20;
+    swap(x, y); // Pass the addresses of x and y
+    return 0;
+}
