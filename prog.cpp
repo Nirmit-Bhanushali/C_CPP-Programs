5a14200c5ef633aac61a690bed40c9ecb124824a
Nirmit <nirmit.bhanushali@gmail.com>
2026-03-04 09:45:38 +0530

    add prog.cpp implementing a swap function in C++

---
+++ prog.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+void swap(int a, int b)
+{
+    cout << "Before swapping a=" << a << " b=" << b;
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "\nAfter swapping a=" << a << " b=" << b;
+}
+int main()
+{
+    int x = 10, y = 20;
+    swap(x, y);
+    return 0;
+}
