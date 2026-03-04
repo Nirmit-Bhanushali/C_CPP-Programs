b0339baa22a5a187996672bfbf19ceba83af8dd8
Nirmit <nirmit.bhanushali@gmail.com>
2026-03-04 11:57:28 +0530

    add Array2D.cpp to read and display values in a 3x3 array

---
+++ Array2D.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[3][3];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "Enter the value for arr[" << i << "][" << j << "]:" << endl;
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr[i][j] << "\t";
+        }
+        cout << "\n\n";
+    }
+    return 0;
+}
