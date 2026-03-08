80b2994b09ce2a2dad937039b3ecc709283c5447
Nirmit-Bhanushali <102681894+Nirmit-Bhanushali@users.noreply.github.com>
2026-03-08 23:59:14 +0530

    Add C++ program to demonstrate basic input and output

--- Variable.cpp
+++ Variable.cpp
@@ -3,11 +3,33 @@ using namespace std;
 
 int main()
 {
+    int myNum;
+    double myFloatNum;
+    char myLetter;
+    string myText;
+    bool myBoolean;
 
-    int myNum = 5;            // Integer (whole number without decimals)
-    double myFloatNum = 5.99; // Floating point number (with decimals)
-    char myLetter = 'D';      // Character
-    string myText = "Hello";  // String (text)
-    bool myBoolean = true;    // Boolean (true or false)
+    cout << "Enter an integer: ";
+    cin >> myNum;
+
+    cout << "Enter a decimal number: ";
+    cin >> myFloatNum;
+
+    cout << "Enter a character: ";
+    cin >> myLetter;
+
+    cout << "Enter a word: ";
+    cin >> myText;
+
+    cout << "Enter 1 for true or 0 for false: ";
+    cin >> myBoolean;
+
+    cout << "\nYou entered:\n";
+    cout << myNum << endl;
+    cout << myFloatNum << endl;
+    cout << myLetter << endl;
+    cout << myText << endl;
+    cout << myBoolean << endl;
+
     return 0;
 }
